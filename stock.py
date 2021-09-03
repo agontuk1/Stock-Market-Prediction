ea33b10fb8788cd1f34b4090396334a2be2f4b22
agontuk1 <69851340+agontuk1@users.noreply.github.com>
2021-09-03 17:53:16 +0530

    Add files via upload

---
+++ stock.py
@@ -0,0 +1,62 @@
+import pandas as pd
+apple = pd.read_csv("AAPL.csv")
+print(apple.head())
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+plt.figure(figsize=(10, 4))
+plt.title("Apple's Stock Price")
+plt.xlabel("Days")
+plt.ylabel("Close Price USD ($)")
+plt.plot(apple["Close Price"])
+plt.show()
+apple = apple[["Close Price"]]
+print(apple.head())
+futureDays = 25
+apple["Prediction"] = apple[["Close Price"]].shift(-futureDays)
+print(apple.head())
+print(apple.tail())
+import numpy as np
+x = np.array(apple.drop(["Prediction"], 1))[:-futureDays]
+print(x)
+y = np.array(apple["Prediction"])[:-futureDays]
+print(y)
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.25)
+# Creating the decision tree regressor model
+from sklearn.tree import DecisionTreeRegressor
+tree = DecisionTreeRegressor().fit(xtrain, ytrain)
+
+# creating the Linear Regression model
+from sklearn.linear_model import LinearRegression
+linear = LinearRegression().fit(xtrain, ytrain)
+xfuture = apple.drop(["Prediction"], 1)[:-futureDays]
+xfuture = xfuture.tail(futureDays)
+xfuture = np.array(xfuture)
+print(xfuture)
+treePrediction = tree.predict(xfuture)
+print("Decision Tree prediction =",treePrediction)
+linearPrediction = linear.predict(xfuture)
+print("Linear regression Prediction =",linearPrediction)
+predictions = treePrediction
+valid = apple[x.shape[0]:]
+valid["Predictions"] = predictions
+plt.figure(figsize=(10, 6))
+plt.title("Apple's Stock Price Prediction Model(Decision Tree Regressor Model)")
+plt.xlabel("Days")
+plt.ylabel("Close Price USD ($)")
+plt.plot(apple["Close Price"])
+plt.plot(valid[["Close Price", "Predictions"]])
+plt.legend(["Original", "Valid", "Predictions"])
+plt.show()
+predictions = linearPrediction
+valid = apple[x.shape[0]:]
+valid["Predictions"] = predictions
+plt.figure(figsize=(10, 6))
+plt.title("Apple's Stock Price Prediction Model(Linear Regression Model)")
+plt.xlabel("Days")
+plt.ylabel("Close Price USD ($)")
+plt.plot(apple["Close Price"])
+plt.plot(valid[["Close Price", "Predictions"]])
+plt.legend(["Original", "Valid", "Predictions"])
+plt.show()
